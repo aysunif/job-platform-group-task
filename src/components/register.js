38a5195cef06c4552e2b717976c431178e37d4b9
Me4hdiyeva <mehdiyevasevda004@gmail.com>
2024-12-03 02:09:27 +0400

    take-reg finish

--- src/components/register.js
+++ src/components/register.js
@@ -7,6 +7,23 @@ console.log(confirmPasswordSearch);
 const btnRegSearch = document.querySelector("#btn-reg-search")
 const emailSearch = document.querySelector("#email-search")
 console.log(emailSearch);
+const usernameTake = document.querySelector("#username-take")
+console.log(usernameTake);
+const emailTake = document.querySelector("#email-take");
+console.log(emailTake);
+const passwordTake = document.querySelector("#password-take");
+const confirmPasswordTake = document.querySelector("#confirmPassword-take");
+const takeReg = document.querySelector("#take-reg")
+console.log(takeReg);
+
+const allForms = document.querySelectorAll("form");
+for (let i = 0; i < allForms.length; i++) {
+  const form = allForms[i];
+
+  form.addEventListener("submit", (e) => {    
+    e.preventDefault();
+  });
+}
 
 
 btnRegSearch.addEventListener("click", () => {
@@ -112,13 +129,122 @@ btnRegSearch.addEventListener("click", () => {
     
     
     }
-
-   
-  
-
-
-
-
 })
+
+
+
+takeReg.addEventListener("click", () => {
+
+  if (usernameTake.value.trim() === "" || passwordTake.value.trim() === "" || confirmPasswordTake.value.trim() === "") {
+      Swal.fire({
+          title: "Input boshdur   ",
+          showClass: {
+            popup: `
+              animate__animated
+              animate__fadeInUp
+              animate__faster
+            `
+          },
+          hideClass: {
+            popup: `
+              animate__animated
+              animate__fadeOutDown
+              animate__faster
+            `
+          }
+        });
+  } else {
+      let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      function validateEmail(email) {
+        if (emailRegex.test(email)) {
+           let passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+      function validatePassword(password) {
+          if (passwordRegex.test(password)) {
+            
+      if (passwordTake.value !== confirmPasswordTake.value) {
+          Swal.fire({
+              title: "Password eyni deil!",
+              showClass: {
+                popup: `
+                  animate__animated
+                  animate__fadeInUp
+                  animate__faster
+                `
+              },
+              hideClass: {
+                popup: `
+                  animate__animated
+                  animate__fadeOutDown
+                  animate__faster
+                `
+              }
+            });
+      } else {
+          let username = usernameTake.value;
+          let email = emailTake.value;
+          let password = passwordTake.value
+          console.log("eynidir"); 
+          localStorage.setItem("ishgoturen-username", username);
+          localStorage.setItem("ishgoturen-email", email);
+          localStorage.setItem("ishgoturen-password", password);
+      }
+          } else {
+              Swal.fire({
+                  title: "Şifrə uyğun deyil!",
+                  showClass: {
+                    popup: `
+                      animate__animated
+                      animate__fadeInUp
+                      animate__faster
+                    `
+                  },
+                  hideClass: {
+                    popup: `
+                      animate__animated
+                      animate__fadeOutDown
+                      animate__faster
+                    `
+                  }
+                });
+              }
+              
+          }
+          validatePassword(passwordTake.value)
+        } else {
+          Swal.fire({
+              title: "Mail formati duzgun deil",
+              showClass: {
+                popup: `
+                  animate__animated
+                  animate__fadeInUp
+                  animate__faster
+                `
+              },
+              hideClass: {
+                popup: `
+                  animate__animated
+                  animate__fadeOutDown
+                  animate__faster
+                `
+              }
+            });
+        }
+      }
+      validateEmail(emailTake.value)
+    
+
+  
+  
+  }
+})
+
+
+
+
+
+
+
+
+
 
 

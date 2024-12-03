7db6a33613f1335f2d88aa4be9bbcc4d8dd61e6c
idris mamedov <idrismamedov04@gmail.com>
2024-12-03 18:19:02 +0400

    companyDetail page created

---
+++ src/services/companyDetails.js
@@ -0,0 +1,26 @@
+// company-details.js
+const BASE_URL = "https://wooded-trusted-trawler.glitch.me";
+const urlParams = new URLSearchParams(window.location.search);
+const companyId = urlParams.get("id");
+
+async function fetchCompanyDetails() {
+  try {
+    const response = await axios.get(`${BASE_URL}/companies/${companyId}`);
+    const company = response.data;
+    console.log(company);
+    renderCompanyDetails(company);
+  } catch (error) {
+    console.error("Error fetching company details:", error);
+  }
+}
+
+function renderCompanyDetails(company) {
+  document.getElementById("company-image").src = company.companyImage;
+  document.getElementById("company-name").textContent = company.name;
+  document.getElementById("company-location").innerHTML = `<strong>Location:</strong> ${company.location}`;
+  document.getElementById("company-industry").innerHTML = `<strong>Industry:</strong> ${company.industry}`;
+  document.getElementById("company-created").innerHTML = `<strong>Created At:</strong> ${new Date(company.createdAt).toLocaleDateString()}`;
+  document.getElementById("company-website").href = company.website;
+}
+
+fetchCompanyDetails();
